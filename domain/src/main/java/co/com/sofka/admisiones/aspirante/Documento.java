75a7dbc07b7b08c2781a0568e50c29d0dd37fdc9
juanManuelMontoya <juanpirry1396@hotmail.com>
2022-06-17 17:50:01 -0500

    update

--- domain/src/main/java/co/com/sofka/admisiones/aspirante/Documento.java
+++ domain/src/main/java/co/com/sofka/admisiones/aspirante/Documento.java
@@ -1,8 +1,8 @@
 package co.com.sofka.admisiones.aspirante;
 
 import co.com.sofka.admisiones.aspirante.values.DocumentoId;
-import co.com.sofka.admisiones.aspirante.values.Nombre;
 import co.com.sofka.admisiones.values.Contenido;
+import co.com.sofka.admisiones.values.Nombre;
 import co.com.sofka.domain.generic.Entity;
 
 public class Documento extends Entity<DocumentoId> {
--- domain/src/main/java/co/com/sofka/admisiones/prueba/Materia.java
+++ domain/src/main/java/co/com/sofka/admisiones/prueba/Materia.java
@@ -1,5 +1,16 @@
 package co.com.sofka.admisiones.prueba;
 
-public class Materia {
-    
+import co.com.sofka.admisiones.prueba.values.Contenido;
+import co.com.sofka.admisiones.prueba.values.MateriaId;
+import co.com.sofka.admisiones.prueba.values.Puntaje;
+import co.com.sofka.domain.generic.Entity;
+
+public class Materia extends Entity<MateriaId> {
+
+    protected Contenido contenido;
+    protected Puntaje puntaje;
+
+    public Materia(MateriaId entityId) {
+        super(entityId);
+    }
 }
