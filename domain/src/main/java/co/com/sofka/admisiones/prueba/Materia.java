699c89e9077163e3e98e2ffb86153ad2ab2a86a9
juanManuelMontoya <juanpirry1396@hotmail.com>
2022-06-17 18:13:38 -0500

    Update

--- domain/src/main/java/co/com/sofka/admisiones/prueba/Materia.java
+++ domain/src/main/java/co/com/sofka/admisiones/prueba/Materia.java
@@ -1,16 +1,38 @@
 package co.com.sofka.admisiones.prueba;
 
-import co.com.sofka.admisiones.prueba.values.Contenido;
 import co.com.sofka.admisiones.prueba.values.MateriaId;
-import co.com.sofka.admisiones.prueba.values.Puntaje;
+import co.com.sofka.admisiones.values.Contenido;
+import co.com.sofka.admisiones.values.Nombre;
 import co.com.sofka.domain.generic.Entity;
+
+import java.util.Objects;
 
 public class Materia extends Entity<MateriaId> {
 
     protected Contenido contenido;
-    protected Puntaje puntaje;
+    protected Nombre nombre;
 
-    public Materia(MateriaId entityId) {
+    public Materia(MateriaId entityId, Nombre nombre, Contenido contenido) {
         super(entityId);
+        this.nombre = nombre;
+        this.contenido = contenido;
+    }
+
+    public void actualizarContenido(Contenido contenido){
+        Objects.requireNonNull(contenido);
+        this.contenido = contenido;
+    }
+
+    public void cambiarNombre(Nombre nombre){
+        Objects.requireNonNull(nombre);
+        this.nombre = nombre;
+    }
+
+    public Contenido getContenido() {
+        return contenido;
+    }
+
+    public Nombre getNombre() {
+        return nombre;
     }
 }
--- domain/src/main/java/co/com/sofka/admisiones/prueba/Prueba.java
+++ domain/src/main/java/co/com/sofka/admisiones/prueba/Prueba.java
@@ -1,6 +1,11 @@
 package co.com.sofka.admisiones.prueba;
 
+import co.com.sofka.admisiones.aspirante.Aspirante;
+import co.com.sofka.admisiones.aspirante.values.AspiranteId;
+import co.com.sofka.admisiones.comite.ComiteChange;
+import co.com.sofka.admisiones.prueba.events.PruebaCreada;
 import co.com.sofka.admisiones.prueba.events.PuntajeActualizado;
+import co.com.sofka.admisiones.prueba.values.MateriaId;
 import co.com.sofka.admisiones.prueba.values.PruebaId;
 import co.com.sofka.admisiones.prueba.values.Puntaje;
 import co.com.sofka.domain.generic.AggregateEvent;
@@ -11,13 +16,25 @@ import java.util.Objects;
 public class Prueba extends AggregateEvent<PruebaId> {
 
     protected Puntaje puntaje;
+    protected MateriaId materiaId;
+    protected AspiranteId aspiranteId;
 
-    public Prueba(PruebaId entityId) {
+    public Prueba(PruebaId entityId, Puntaje puntaje, MateriaId materiaId, AspiranteId aspiranteId) {
         super(entityId);
+        appendChange(new PruebaCreada(puntaje,materiaId,aspiranteId)).apply();
+    }
+
+    private Prueba(PruebaId pruebaId){
+        super(pruebaId);
+        subscribe(new PruebaChange(this));
     }
 
     public void actualizarPuntaje(Puntaje puntaje){
         Objects.requireNonNull(puntaje);
         appendChange(new PuntajeActualizado(puntaje)).apply();
+    }
+
+    public Puntaje puntaje() {
+        return puntaje;
     }
 }
